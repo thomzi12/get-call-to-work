7807f0d90f7cb16997f0a22c7f67dff8d4587ab9
Zachary Thomas <zthomas.nc@gmail.com>
2024-04-02 17:44:30 -0400

    Add Youtube video parsing function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,10 @@ export default function Home() {
   const [inputText, setInputText] = useState('');
   const [isClicked, setIsClicked] = useState(false);
 
+  function getVideoId(link: string): string {
+    return link.includes("?v=") ? link.split("?v=")[1] : link.split("?si=")[1];
+  }
+
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputText(event.target.value);
     setIsClicked(false);
@@ -13,6 +17,8 @@ export default function Home() {
   const handleClick = () => {
     setIsClicked(true);
   };
+
+  const videoId = getVideoId(inputText);
 
   return (
     <main>
@@ -23,6 +29,7 @@ export default function Home() {
         onChange={handleInputChange}
         className="my-input-box"
       /><br />
+      <p>Here's the video Id of the user input: {videoId}</p><br />
       <p>When user clicks this button, it should fetch a Youtube URL and display it below:</p><br />
       <button className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30" onClick={handleClick}>Get transcript button</button>
     </main>
