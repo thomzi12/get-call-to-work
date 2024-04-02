2eb376ba10f791539af86d933cd0d63a65c1c855
Zachary Thomas <zthomas.nc@gmail.com>
2024-04-02 17:40:26 -0400

    Put logic within Home()

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,30 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 
-const [inputText, setInputText] = useState('');
-const [isClicked, setIsClicked] = useState(false);
-
-const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  setInputText(event.target.value);
-  setIsClicked(false);
-};
-
-const handleClick = () => {
-  setIsClicked(true);
-};
-
-
 export default function Home() {
+  const [inputText, setInputText] = useState('');
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value);
+    setIsClicked(false);
+  };
+
+  const handleClick = () => {
+    setIsClicked(true);
+  };
+
   return (
     <main>
-     <p>User gives a youtube link (let's assume it's valid)</p><br/>
-     <input
-          type="text"
-          value={inputText}
-          onChange={handleInputChange}
-          className="my-input-box"
-        /><br/>
-    <p>When user clicks this button, it should fetch a Youtube URL and display it below:</p><br/>
-    <button onClick={handleClick}>Get transcript</button>
+      <p>User gives a youtube link (let's assume it's valid)</p><br />
+      <input
+        type="text"
+        value={inputText}
+        onChange={handleInputChange}
+        className="my-input-box"
+      /><br />
+      <p>When user clicks this button, it should fetch a Youtube URL and display it below:</p><br />
+      <button onClick={handleClick}>Get transcript</button>
     </main>
   );
 }
